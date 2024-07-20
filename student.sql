5252937d57636f37a407bcf3184e4d4b2a0fe119
kundetigowtham06 <152044248+kundetigowtham06@users.noreply.github.com>
2024-07-20 16:42:18 +0530

    Create student.sql

---
+++ student.sql
@@ -0,0 +1,14 @@
+create table student (rollno number(4) primary key,name varchar(20) not null,age number(2),Bname varchar(5),perc number(5,2),email varchar(20) unique);
+insert into student values(420,'Gowtham',18,'Cse',96.23,'ksngowtham@gmail.com');
+insert into student values(421,'Deepak',19,'ece',95.23,'deepak@gmail.com');
+insert into student values(422,'Anuram',17,'Cse',94.23,'anuram@gmail.com');
+select * from student;
+select rollno, name, age,Bname,perc, email from student where Bname='Cse';
+select * from student order by rollno desc;
+alter table student add cgpa number(4,2);
+select * from student;
+update student  set cgpa=8.62 where rollno=421;
+select * from student;
+select min(perc) from student;
+select max(perc) from student;
+
